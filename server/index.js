43564714122d799dbd17770ef0b61e98e15321dd
Bishnu Dev <devbishnu2000@gmail.com>
2020-03-07 11:55:33 +0545

    setting up for deployment

--- server/index.js
+++ server/index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require("path");
 const config = require('../config/index');
 
 var entriesRoute = require('./routes/entries');
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '..', 'client/build')));
 
-app.use(bodyParser.json()); 
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', entriesRoute);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '/client/build/index.html'));
+});
 
 const port = config.backend_port;
 
